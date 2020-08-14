2e282ca5ceff646ec6f2e7fa62c477dea3166561
octavio <octaviosoto777@gmail.com>
2020-08-14 13:30:41 -0500

    Added routes for resource task, dashboard and home

--- taskmanager/routes/web.php
+++ taskmanager/routes/web.php
@@ -17,6 +17,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::get('/task', 'TaskController@index')->name('task.index');
+Route::get('/task/create', 'TaskController@create')->name('task.create');
+Route::post('/task', 'TaskController@store')->name('task.store');
+Route::get('/task/edit/{id}', 'TaskController@edit')->name('task.edit');
+Route::patch('/task/{id}', 'TaskController@update')->name('task.update');
+Route::delete('/task/{id}', 'TaskController@destroy')->name('task.destroy');
 
+Route::get('/dashboard', 'DashboardController@index')->name('dashboard.index');
+
+
+Auth::routes(['register' => true]);
+
+// Route::get('/dashboard', 'HomeController@index')->name('dashboard.index');
 Route::get('/home', 'HomeController@index')->name('home');
