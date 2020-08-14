daf59185f8fcb7ea7b7881431d4460fea0efe7ff
octavio <octaviosoto777@gmail.com>
2020-08-14 13:30:00 -0500

    Added dashboard resource with controller

---
+++ taskmanager/resources/views/dashboard/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.base')
+
+@section('content')
+<div class="dashboard">
+  <div class="dashboard-box total-users">
+    <h1>{{ $users }}</h1>
+    <h4>Total users</h4>
+  </div>
+  <div class="dashboard-box ongoing-tasks">
+    <h1>{{ $taskCount }}</h1>
+    <h4>On going tasks</h4>
+  </div>
+  <div class="dashboard-box completed-tasks">
+    <h1>{{ $taskCount }}</h1>
+    <h4>Completed tasks</h4>
+  </div>
+  <div class="dashboard-box total-tasks">
+    <h1>{{ $taskCount }}</h1>
+    <h4>Total tasks</h4>
+  </div>
+</div>
+@endsection
