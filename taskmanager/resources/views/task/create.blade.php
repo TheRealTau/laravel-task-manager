7fcd1363c0671020d3351abb799c02e3d2240257
octavio <octaviosoto777@gmail.com>
2020-08-15 12:39:49 -0500

    Added materialize date and time picker for edit and create forms

--- taskmanager/resources/views/task/create.blade.php
+++ taskmanager/resources/views/task/create.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.base')
 
 @section('content')
-<div class="content-wrapper">
-  <div class="crud-form">
-    <h1>New task</h1>
+<div class="container">
+  <div class="container">
+    <h3>New task</h3>
     <form action="{{ route('task.store') }}" method="POST">
       @csrf
       <label for="name">Task name</label>
@@ -12,15 +12,38 @@
 
       <label for="user-select">User asigned:</label>
       <select name="user_id" id="user-select">
+        <option value="" disabled selected>Choose an user</option>
         @foreach ( $users as $user )
           <option value="{{ $user->id }}">{{ $user->name }}</option>
         @endforeach
-      </select>
-      <br>
-      <div class="create-button">
-        <input type="submit" value="Create task">
+      </select><br>
+      {{ $errors->first('user_id') }}<br>
+
+      <div class="row">
+        <div class="input-field col s6">
+          <div class="input-field row s6">
+            <label for="strat_date">Start date</label>
+            <input type="text" class="datepicker"  name="start_date" placeholder="Select start date">
+          </div>
+          <div class="input-field row">
+            <label for="start_time">Start time</label>
+            <input type="text" class="timepicker"  name="start_time" placeholder="Select start time">
+          </div>
+        </div>
+        <div class="input-field col s6">
+          <div class="input-field row s6">
+            <label for="end_date">End date</label>
+            <input type="text" class="datepicker"  name="end_date" placeholder="Select end date">
+          </div>
+          <div class="input-field row">
+            <label for="end_time">End time</label>
+            <input type="text" class="timepicker"  name="end_time" placeholder="Select end time">
+          </div>
+        </div>
       </div>
-      
+      <div class="create-button">
+        <input class="btn light-green" type="submit" value="Create task">
+      </div>
     </form>
     <a href="{{ route('task.index') }}">Return to task list</a>
   </div>
--- taskmanager/resources/views/task/edit.blade.php
+++ taskmanager/resources/views/task/edit.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.base')
 
 @section('content')
-<div class="content-wrapper">
-  <div class="crud-form">
-    <h1>Task editor</h1>
+<div class="container">
+  <div class="container">
+    <h3>Task editor</h3>
     <form action="{{ route('task.update', $task->id) }}" method="POST">
       @csrf
       @method('PATCH')
-      <label for="name">Contact name</label>
+      <label for="name">Task name</label>
       <input type="text" id="name" name="name" value="{{ $task->name }}"><br>
       {{ $errors->first('name') }}<br>
 
@@ -20,8 +20,31 @@
           @endif
         @endforeach
       </select><br>
+      {{ $errors->first('user_id') }}<br>
+      <div class="row">
+        <div class="input-field col s6">
+          <div class="input-field row s6">
+            <label for="strat_date">Start date</label>
+            <input type="text" class="datepicker"  name="start_date" value="{{ $start_date }}">
+          </div>
+          <div class="input-field row">
+            <label for="start_time">Start time</label>
+            <input type="text" class="timepicker"  name="start_time" value="{{ $start_time }}">
+          </div>
+        </div>
+        <div class="input-field col s6">
+          <div class="input-field row s6">
+            <label for="end_date">End date</label>
+            <input type="text" class="datepicker"  name="end_date" value="{{ $end_date }}">
+          </div>
+          <div class="input-field row">
+            <label for="end_time">End time</label>
+            <input type="text" class="timepicker"  name="end_time" value="{{ $end_time}}">
+          </div>
+        </div>
+      </div>
       <div class="create-button">
-        <input type="submit" value="Update task">
+        <input type="submit" value="Update task" class="btn light-green">
       </div>
     </form>
     <a href="{{ route('task.index') }}">Return to task list</a>
