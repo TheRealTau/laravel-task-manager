c93f8b1dfb921690ff61dccd9108f2a8a4d0ad11
octavio <octaviosoto777@gmail.com>
2020-08-15 18:29:48 -0500

    updated simple home page

--- taskmanager/resources/views/welcome.blade.php
+++ taskmanager/resources/views/welcome.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.base')
 
 @section('content')
-<div class="flex-center position-ref">
-  <div class="content">
-    <div class="title m-b-md">
+  <div class="container">
+    <div class="container title-app">
       <h1>Task Manager App</h1>
+      <h6>Log in to use the app</h6>
     </div>
   </div>
-</div>
 @endsection
