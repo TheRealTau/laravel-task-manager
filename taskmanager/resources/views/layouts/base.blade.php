c9cfed7765a7ff182b29b715d475a3175dc08ab8
octavio <octaviosoto777@gmail.com>
2020-08-15 12:39:16 -0500

    Added materialize to the project and updates nevbar to new style

--- taskmanager/resources/views/layouts/base.blade.php
+++ taskmanager/resources/views/layouts/base.blade.php
@@ -19,75 +19,73 @@
   <!-- Styles -->
   <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 
-  {{-- MATERIALIZE --}}
-  <!-- Compiled and minified CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-  <!-- Compiled and minified JavaScript -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-          
 </head>
 <body>
-  <div id="main-container"> 
-    <nav class="navbar-container">
-      <div class="navbar-sections">
-        <div class="navbar-left">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="{{ url('/') }}">Home</a>
-            </li>
-            {{-- @auth --}}
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('dashboard.index') }}">Dashboard</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('task.index') }}">Tasks</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="">Users</a>
-              </li>
-              {{-- @endauth --}}
-          </ul>
-        </div>
+  <div id="main-container">
+    <nav>
+      <div class="nav-wrapper grey darken-3">
+        
+        <ul id="nav-mobile" class="left hide-on-med-and-down">
+          {{-- <li><a href="#" class="brand-logo">Home</a></li> --}}
+          <li><a href="{{ route('dashboard.index') }}">Dashboard</a></li>
+          <li><a href="{{ route('task.index') }}">Tasks</a></li>
+          <li><a href="{{ route('user.index') }}">Users</a></li>
+        </ul>
 
-        <div class="navbar-right">
-          <ul class="navbar-nav ml-auto">
-            <!-- Authentication Links -->
-            @guest
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <!-- Authentication Links -->
+          @guest
+            <li>
+              <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            @if (Route::has('register'))
+              <li>
+                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
               </li>
-              @if (Route::has('register'))
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                </li>
-              @endif
-            @else
-              <li class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                  {{ Auth::user()->name }} <span class="caret"></span>
+            @endif
+          @else
+            <li>
+              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }} <span class="caret"></span>
+              </a>
+
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                              document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
                 </a>
 
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="{{ route('logout') }}"
-                      onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
-                          {{ __('Logout') }}
-                  </a>
-
-                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                  </form>
-                </div>
-              </li> 
-            @endguest
-          </ul>
-        </div>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+                </form>
+              </div>
+            </li> 
+          @endguest
+        </ul>
       </div>
     </nav>
-    <div class="content-container">
+
+    <div>
       @yield('content')
     </div>
   </div>
+
+    {{-- MATERIALIZE --}}
+    {{-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    {{-- <script src="{{ asset('js/init.js') }}"></script> --}}
+
+    <script>
+      $('select').formSelect();
+      $('.datepicker').datepicker({
+        autoClose : true,
+        format : 'yyyy-mm-dd'});
+      $('.timepicker').timepicker();
+      $('.collapsible').collapsible();
+    </script>
 </body>
 </html>
