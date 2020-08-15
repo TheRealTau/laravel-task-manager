cbebc294c3f1454b86a2a72841686bbd0f05769c
octavio <octaviosoto777@gmail.com>
2020-08-15 12:40:47 -0500

    Update index view for task resource

--- taskmanager/resources/views/task/index.blade.php
+++ taskmanager/resources/views/task/index.blade.php
@@ -3,30 +3,30 @@
 @section('content')
 <div>
   <div class="task-list-header">
-    <h1>Task List:</h1>
-    <a href="{{ route('task.create') }}"><button>Add new task</button></a>
+    <h3>Task List:</h3>
+    <a class="btn light-green" href="{{ route('task.create') }}">Add new task</a>
   </div>
   
-  <p class="mssg">{{ session('mssg') }}</p>
+  {{-- <p class="mssg">{{ session('mssg') }}</p> --}}
   <div class="task-list">
     @foreach ($tasks as $task)
       <div class="task">
         <div class="task-header">
-          <h1> {{ $task->name }} {{ $task->last_name }}</h1>
+          <h4> {{ $task->name }} {{ $task->last_name }}</h4>
           {{-- <h3>Group: {{ $contact->group ? $contact->group->name : 'No group asigned' }}</h3> --}}
           <div class="task-options">
-            <a href="{{ route('task.edit', $task->id) }}"><button>Edit task</button></a>
+            <a class="btn orange" href="{{ route('task.edit', $task->id) }}">Edit</a>
             <form action="{{ route('task.destroy', $task->id) }}" method="POST">
               @csrf
               @method('DELETE')
-              <button>Delete task</button>
+              <button class="btn red">Delete</button>
             </form>
           </div>
         </div>
         
-        <h4>Asigned to: {{ $task->user->name }}</h4>
-        <h5>Start at: {{ $task->start_at }}</h5>
-        <h5>End at: {{ $task->end_at }}</h5>
+        <h5>Asigned to: {{ $task->user->name }}</h5>
+        <h6>Start at: {{ $task->start_at }}</h6>
+        <h6>End at: {{ $task->end_at }}</h6>
       </div>
     @endforeach
   </div>
