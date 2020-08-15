588e24e49dcfade900350845bec46fba5b18f20f
octavio <octaviosoto777@gmail.com>
2020-08-15 18:25:26 -0500

    now require auth to access this resources

--- taskmanager/app/Http/Controllers/DashboardController.php
+++ taskmanager/app/Http/Controllers/DashboardController.php
@@ -8,6 +8,11 @@ use App\Task;
 
 class DashboardController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $users = User::orderBy('name', 'asc')->get()->count();
