7f07b726c3a571721c9311be51a356669a669faa
octavio <octaviosoto777@gmail.com>
2020-08-15 16:38:59 -0500

    Dashboard controller now return users count and task count for: total tasks, completed tasks and on going tasks

--- taskmanager/app/Http/Controllers/DashboardController.php
+++ taskmanager/app/Http/Controllers/DashboardController.php
@@ -11,73 +11,15 @@ class DashboardController extends Controller
     public function index()
     {
         $users = User::orderBy('name', 'asc')->get()->count();
-        $taskCount = Task::all()->count();
-        return view('dashboard.index', ['users'=>$users, 'taskCount'=>$taskCount]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $task = Task::all();
+        $taskCount = $task->count();
+        $taskComplete = $task->where('complete', 1)->count();
+        $taskOngoing = $task->where('complete', 0)->count();
+        return view('dashboard.index', [
+            'users'=>$users,
+            'taskCount'=>$taskCount,
+            'taskComplete'=>$taskComplete,
+            'taskOngoing'=>$taskOngoing
+        ]);
     }
 }
