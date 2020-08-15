cd47787c51042d558dcda14cd798254f1fc70bd2
octavio <octaviosoto777@gmail.com>
2020-08-15 16:49:18 -0500

    Updated style for task element

--- taskmanager/resources/views/task/index.blade.php
+++ taskmanager/resources/views/task/index.blade.php
@@ -2,18 +2,17 @@
 
 @section('content')
 <div>
-  <div class="task-list-header">
-    <h3>Task List:</h3>
+  <div class="list-header">
+    <h3>Task list</h3>
     <a class="btn light-green" href="{{ route('task.create') }}">Add new task</a>
   </div>
   
   {{-- <p class="mssg">{{ session('mssg') }}</p> --}}
   <div class="task-list">
     @foreach ($tasks as $task)
-      <div class="task">
+      <div class="task {{ $task->complete === 1 ? 'task-complete' : 'task-ongoing' }}">
         <div class="task-header">
-          <h4> {{ $task->name }} {{ $task->last_name }}</h4>
-          {{-- <h3>Group: {{ $contact->group ? $contact->group->name : 'No group asigned' }}</h3> --}}
+          <h6> {{ $task->name }} <small> ( {{ $task->complete === 0 ? 'On going' : 'Completed' }} )</small></h6>
           <div class="task-options">
             <a class="btn orange" href="{{ route('task.edit', $task->id) }}">Edit</a>
             <form action="{{ route('task.destroy', $task->id) }}" method="POST">
@@ -23,10 +22,11 @@
             </form>
           </div>
         </div>
-        
-        <h5>Asigned to: {{ $task->user->name }}</h5>
-        <h6>Start at: {{ $task->start_at }}</h6>
-        <h6>End at: {{ $task->end_at }}</h6>
+        <div class="task-body">
+          <h5>Assigned to: {{ $task->user->name }}</h5><br>
+          <h6>Start at: {{ $task->start_at }}</h6>
+          <h6>End at: {{ $task->end_at }}</h6>
+        </div>
       </div>
     @endforeach
   </div>
